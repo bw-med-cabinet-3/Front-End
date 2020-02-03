a1dd1e524c097ddf9498cc3dd251dc13ec5a6657
carlsachs <sachscarl@gmail.com>
2020-02-03 13:12:12 -0500

    added container and card component

---
+++ med-cabinet/src/components/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+        This is the card that the strains will appear on.
+        </div>
+    )
+}
---
+++ med-cabinet/src/components/Container.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Container = () => {
+    return (
+        <div>
+        <h1>this is the page for listing strains and searching them</h1>
+        </div>
+    )
+}
