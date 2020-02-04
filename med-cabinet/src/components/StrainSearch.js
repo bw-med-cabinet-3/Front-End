9f68aa303441ca1e6eb0e41e52a05041fe9ffd2b
carlsachs <sachscarl@gmail.com>
2020-02-04 15:37:26 -0500

    changes to strain search page

--- med-cabinet/src/components/StrainSearch.js
+++ med-cabinet/src/components/StrainSearch.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./Nav";
+// import { axiosWithAuth } from "../utils/axiosWithAuth";
+import axios from "axios";
+import Card from "./Card";
 
 const StrainSearch = () => {
+
+    const [strains, setStrains] = useState([]);
+
+    useEffect(() =>{
+        axios.get(``)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log("There's an error", err);
+        });
+    }, []);
+
+
     return (
         <div>
         <Nav />
-        <h1>this is the page for listing strains and searching them</h1>
+        <h2>this is the page for listing strains and searching them</h2>
         </div>
     )
 }
