dc4d5022ea7219a7d7f049bf25108003f4a0c3bb
carlsachs <sachscarl@gmail.com>
2020-02-04 14:39:22 -0500

    Basic layout of card added

--- med-cabinet/src/components/Card.js
+++ med-cabinet/src/components/Card.js
@@ -3,10 +3,16 @@ import Nav from "./Nav";
 
 const Card = () => {
     return (
-        <div>
-        <Nav />
-        <h2>This is the card that the strains will appear on.</h2>
-        </div>
+        <Card>
+            <Nav />
+            <h1>Strain:</h1>
+            <h2>Type:</h2>
+            <h2>Rating:</h2>
+            <h2>Effects:</h2>
+            <h2>Flavors:</h2>
+            <h2>Symptoms:</h2>
+            <p>Description:</p>
+        </Card>
     )
 }
 export default Card;
--- med-cabinet/src/components/Search.js
+++ med-cabinet/src/components/Search.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Search = () => {
     return (
-        <div>This is the search</div>
+        <div>
+        This is the search
+        </div>
     )
 }
 export default Search;
