c92a03486968334924aed3120e1f99898b202791
carlsachs <sachscarl@gmail.com>
2020-02-04 16:17:18 -0500

    added axios to profile

--- med-cabinet/src/components/Profile.js
+++ med-cabinet/src/components/Profile.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ProfileCard from "./ProfileCard";
+import axios from "axios";
 
 export default function Profile() {
+
+    useEffect(() => {
+    axios.get(``)
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.log("There's an error fetching user data", err)
+    });
+}, []);
+
     return (
         <div>
         <h2>ProfilePage</h2>
