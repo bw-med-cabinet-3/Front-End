c29fc3e4bc9c682892147e72c3ff8dd69e08171c
carlsachs <sachscarl@gmail.com>
2020-02-05 00:59:24 -0500

    added icons

--- med-cabinet/src/components/Card.js
+++ med-cabinet/src/components/Card.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+//Icon Import
+import Star from "./img/star.png"
+
 
 const Card = (props) => {
 
@@ -18,18 +21,41 @@ console.log(props);
         h2 {
             margin: 0 auto;
             margin-bottom: 5%;
-            margin-top: 3%;
+            margin-top: 5%;
         }
         p {
             margin: 0 auto;
             margin-bottom: 4%;
+            padding-left: 4%;
+            padding-right: 4%;
+        }
+        h3 {
+            margin: 0 auto;
         }
     `;
+
+    const Rate = styled.div`
+        display:flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+        margin-top: 2%;
+        img {
+            margin-left 4%;
+        }
+        h4 {
+            margin-top: 1%;
+        }
+    `;
+
     return (
         <Box>
             <h2>Strain: {props.name}</h2>
             <p>Type: {props.type}</p>
-            <p>Rating: {props.rating}</p>
+            <h3>Rating:</h3>
+            <Rate> 
+                <h4>{props.rating}</h4>
+                <img src={Star} alt="icon for rating"/>
+            </Rate>
             <p>Effects:{props.effects}</p>
             <p>Flavors: {props.flavors}</p>
             <p>Description: {props.description}</p>
