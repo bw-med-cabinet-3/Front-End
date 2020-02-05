7225a0fc6592bf004aa3ae62dfae07ec76639776
carlsachs <sachscarl@gmail.com>
2020-02-05 18:31:18 -0500

    added some hover effects

--- med-cabinet/src/components/LandingPage.js
+++ med-cabinet/src/components/LandingPage.js
@@ -22,21 +22,37 @@ const LandingPage = () => {
   `;
 
   const ButtonWrap = styled.div`
-    width: 50%;
+    width: 70%;
     height: 300px;
     display:flex;
+    justify-content: space-between;
     margin: 0 auto;
     margin-top: 15%;
     flex-flow: row nowrap;
   `;
   const Login = styled.button`
-    width: 50%;
-    background: white;
-  `;
-  const Register = styled.button`
-    width: 50%;
+    width: 30%;
     background: white;
     border: 1px solid green;
+    border-radius: 5%;
+    :hover {
+      box-shadow: 0 4px 8px 0 rgba( 12, 102, 7, 0.2), 0 6px 20px 0 rgba( 12, 102, 7, 0.19);
+      border:none;
+      background: rgb(59,161,76);
+      background: radial-gradient(circle, rgba(59,161,76,0) 0%, rgba(105,215,123,1) 49%, rgba(25,156,62,0) 100%, rgba(21,164,65,1) 100%);
+    }
+  `;
+  const Register = styled.button`
+    width: 30%;
+    background: white;
+    border: 1px solid green;
+    border-radius: 5%;
+    :hover {
+      box-shadow: 0 4px 8px 0 rgba( 12, 102, 7, 0.2), 0 6px 20px 0 rgba( 12, 102, 7, 0.19);
+      border:none;
+      background: rgb(59,161,76);
+      background: radial-gradient(circle, rgba(59,161,76,0) 0%, rgba(105,215,123,1) 49%, rgba(25,156,62,0) 100%, rgba(21,164,65,1) 100%);
+    }
   `;
 
   return (
