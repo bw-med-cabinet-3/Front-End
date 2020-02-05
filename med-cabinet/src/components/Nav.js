44ccc7aa742735414def8bfe3d9f96c33c20acb5
carlsachs <sachscarl@gmail.com>
2020-02-05 02:47:07 -0500

    added a green tint to the navbar box shadow via RGBA

--- med-cabinet/src/components/Nav.js
+++ med-cabinet/src/components/Nav.js
@@ -12,7 +12,7 @@ export default function Nav () {
             color: green;
             text-decoration: none;
             border-radius: 7%;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            box-shadow: 0 4px 8px 0 rgba( 12, 102, 7, 0.2), 0 6px 20px 0 rgba( 12, 102, 7, 0.19);
             transition: all 0.3s ease;
         }
     `;
