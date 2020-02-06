eaee856e74cf66f535118fa5f68a9d18d8113ddc
carlsachs <sachscarl@gmail.com>
2020-02-05 22:23:28 -0500

    adding to registration the BG, and debugging

--- med-cabinet/src/components/Register.js
+++ med-cabinet/src/components/Register.js
@@ -5,9 +5,30 @@ import { useHistory } from 'react-router-dom';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 // Styling
 import { Button, Form, FormGroup, Input } from 'reactstrap';
+//Import Particles-JS
+import Particles from 'react-particles-js';
+import styled from "styled-components";
 
 
 const Register = () => {
+  //Particles
+  const particleOpt = {
+    "particles":{"number":{"value":80,"density":{"enable":true,"value_area":1000}},"color":{"value":"#80ffaa"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":.5,"random":false,"anim":{"enable":false,"speed":.5,"opacity_min":0.1,"sync":false}},"size":{"value":12,"random":true,"anim":{"enable":false,"speed":60,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":250,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":4,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
+    "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":350,"line_linked":{"opacity":1}},
+    "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":400,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
+    "retina_detect":true,
+    "position": "absolute"
+  }
+
+  const Page = styled.div`
+  width:100%;
+  height: 150vh;
+  background: rgb(57,195,97);
+  background: radial-gradient(circle, rgba(57,195,97,1) 0%, rgba(165,255,188,1) 100%);
+  `;
+  const Register = styled.div`
+    border: 1px solid black;
+  `;
   // Set initial state for credentials, fetch check and error
   const [credentials, setCredentials] = useState({
     first_name: '',
@@ -40,7 +61,8 @@ const Register = () => {
 
 
   return (
-    <div className="register">
+    <Page>
+    <Register>
       <div>
         <h1>OH LOOK IT'S A REGISTER</h1>
         <Form onSubmit={register}>
@@ -89,7 +111,14 @@ const Register = () => {
         <p>{isFetching ? 'Loading...' : null}</p>
         <p>{error ? error : null}</p>
       </div>
-    </div>
+    </Register>
+    <Particles 
+    params={ particleOpt }
+     style={{
+      "zIndex": "-1"
+    }}
+    />
+    </Page>
   );
 };
 
