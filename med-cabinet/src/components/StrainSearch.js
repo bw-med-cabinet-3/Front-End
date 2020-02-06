2f87ea1e29f7d49681073e05544f4c6e7f540546
carlsachs <sachscarl@gmail.com>
2020-02-06 14:21:59 -0500

    adding IBG to strain search

--- med-cabinet/src/components/StrainSearch.js
+++ med-cabinet/src/components/StrainSearch.js
@@ -11,6 +11,7 @@ import StrainCard from "./StrainCard";
 import Search from "./Search";
 // Styling
 import "./StrainSearch.css";
+import Particles from "react-particles-js";
 
 
 const StrainSearch = props => {
@@ -41,6 +42,11 @@ const StrainSearch = props => {
     // flex-flow: row wrap;
     // justify-content: space-evenly;
     // `;
+
+    //Setting up IBG:
+    const particleOpt3 = {"particles":{"number":{"value":170,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5211089197812949,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"top-right","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
+    "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
+    "retina_detect":true};
 
     
     const [data, setData] = useState([]);
@@ -76,8 +82,14 @@ const StrainSearch = props => {
 
 
     return (
-        <div className="wrap">
+        <div className="homeWrap">
             <Nav />
+            <Particles 
+            params={ particleOpt3 }
+             style={{
+              "zIndex": "-1"
+            }}
+            />
             <div className="title">
                 <h1>Strains</h1>
             </div>
