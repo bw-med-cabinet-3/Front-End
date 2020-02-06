2bf8107984a60090dbf31fb9224de57ae694c199
carlsachs <sachscarl@gmail.com>
2020-02-05 22:16:08 -0500

    added background to login

--- med-cabinet/src/components/Login.js
+++ med-cabinet/src/components/Login.js
@@ -6,22 +6,28 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 // Styling
 import { Button, Form, FormGroup, Input } from 'reactstrap';
 import styled from "styled-components";
+//Import Particles-JS
+import Particles from 'react-particles-js';
 
 
 const Login = () => {
 
   //SETTING UP IBG
   const particleOpt = {
-    "particles":{"number":{"value":80,"density":{"enable":true,"value_area":1000}},"color":{"value":"#009933"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":.5,"random":false,"anim":{"enable":false,"speed":.5,"opacity_min":0.1,"sync":false}},"size":{"value":12,"random":true,"anim":{"enable":false,"speed":60,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":250,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
-    "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
-    "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
+    "particles":{"number":{"value":80,"density":{"enable":true,"value_area":1000}},"color":{"value":"#80ffaa"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":.5,"random":false,"anim":{"enable":false,"speed":.5,"opacity_min":0.1,"sync":false}},"size":{"value":12,"random":true,"anim":{"enable":false,"speed":60,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":250,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":4,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
+    "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":350,"line_linked":{"opacity":1}},
+    "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":400,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
     "retina_detect":true,
     "position": "absolute"
+
   }
+
 
   const Page = styled.div`
   width: 100%;
-  height: 150vh;
+  height: 110vh;
+  background: rgb(57,195,97);
+  background: radial-gradient(circle, rgba(57,195,97,1) 0%, rgba(165,255,188,1) 100%);
   `;
 
   // Set initial state for credentials, fetch check and error
@@ -83,6 +89,12 @@ const Login = () => {
         <p>{error ? error : null}</p>
       </div>
     </div>
+    <Particles 
+    params={ particleOpt }
+     style={{
+      "zIndex": "-1"
+    }}
+    />
     </Page>
   );
 };
