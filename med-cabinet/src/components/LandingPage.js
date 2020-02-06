069b7f286d0daa73e6b58f596c0fc3e3b5ec4cdc
carlsachs <sachscarl@gmail.com>
2020-02-05 20:38:05 -0500

    landing page completedgit add .!

--- med-cabinet/src/components/LandingPage.js
+++ med-cabinet/src/components/LandingPage.js
@@ -20,38 +20,32 @@ const LandingPage = () => {
 
   //Particles
   const particleOpt = {
-    // particles: {
-    //   number: {
-    //     value: 150,
-    //     density: {
-    //       enable: true,
-    //       value_area: 800
-    //     }
-    //   }
-    // }
-    "particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
+    "particles":{"number":{"value":80,"density":{"enable":true,"value_area":1000}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
     "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
     "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
-    
-    "retina_detect":true
+    "retina_detect":true,
+    "position": "absolute"
+
   }
 
   //styling
   const Page = styled.div`
     width:100%;
-    background-color: red;
+    height: 100vh;
+    background: rgb(80,181,110);
+    background: radial-gradient(circle, rgba(80,181,110,1) 0%, rgba(160,255,163,1) 100%)
   `;
 
   const ButtonWrap = styled.div`
     width: 70%;
-    height: 300px;
+    background: none;
+    height: 200px;
     display:flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
+    padding-top: 3%;
     margin: 0 auto;
-    margin-top: 22%;
+    zIndex: 2;
     flex-flow: row nowrap;
-    z-index: 2;
-    border: 1px solid black;
   `;
   const Login = styled.button`
     width: 30%;
@@ -62,8 +56,8 @@ const LandingPage = () => {
     :hover {
       box-shadow: 0 4px 8px 0 rgba( 12, 102, 7, 0.2), 0 6px 20px 0 rgba( 12, 102, 7, 0.19);
       border:none;
-      background: rgb(59,161,76);
-      background: radial-gradient(circle, rgba(59,161,76,0) 0%, rgba(105,215,123,1) 49%, rgba(25,156,62,0) 100%, rgba(21,164,65,1) 100%);
+      background: rgb(118,255,158);
+      background: radial-gradient(circle, rgba(118,255,158,1) 0%, rgba(255,255,255,1) 100%);
     }
   `;
   const Register = styled.button`
@@ -75,20 +69,22 @@ const LandingPage = () => {
     :hover {
       box-shadow: 0 4px 8px 0 rgba( 12, 102, 7, 0.2), 0 6px 20px 0 rgba( 12, 102, 7, 0.19);
       border:none;
-      background: rgb(59,161,76);
-      background: radial-gradient(circle, rgba(59,161,76,0) 0%, rgba(105,215,123,1) 49%, rgba(25,156,62,0) 100%, rgba(21,164,65,1) 100%);
+      background: rgb(118,255,158);
+      background: radial-gradient(circle, rgba(118,255,158,1) 0%, rgba(255,255,255,1) 100%);
     }
   `;
 
   return (
+    
     <Page>
     <ButtonWrap>
     <Login onClick={toLogin}>Log in</Login>
     <Register onClick={toRegister}>Register</Register>
-  </ButtonWrap>
+    </ButtonWrap>
     <Particles 
-    params={ particleOpt } style={{
-      "z-index": "-1"
+    params={ particleOpt }
+     style={{
+      "zIndex": "-1"
     }}
     />
     </Page>
