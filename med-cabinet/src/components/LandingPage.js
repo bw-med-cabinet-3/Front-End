99559a0e32e75e8c272b27115516f0cf46c1fe7e
carlsachs <sachscarl@gmail.com>
2020-02-05 19:57:05 -0500

    added something special

--- med-cabinet/src/components/LandingPage.js
+++ med-cabinet/src/components/LandingPage.js
@@ -19,21 +19,27 @@ const LandingPage = () => {
   }
 
   //Particles
-  // const particleOpt = {
-  //   particles: {
-  //     number: {
-  //       value: 150,
-  //       density: {
-  //         enable: true,
-  //         value_area: 800
-  //       }
-  //     }
-  //   }
-  // }
+  const particleOpt = {
+    // particles: {
+    //   number: {
+    //     value: 150,
+    //     density: {
+    //       enable: true,
+    //       value_area: 800
+    //     }
+    //   }
+    // }
+    "particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
+    "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
+    "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
+    
+    "retina_detect":true
+  }
 
   //styling
   const Page = styled.div`
     width:100%;
+    background-color: red;
   `;
 
   const ButtonWrap = styled.div`
@@ -44,6 +50,8 @@ const LandingPage = () => {
     margin: 0 auto;
     margin-top: 22%;
     flex-flow: row nowrap;
+    z-index: 2;
+    border: 1px solid black;
   `;
   const Login = styled.button`
     width: 30%;
@@ -74,10 +82,15 @@ const LandingPage = () => {
 
   return (
     <Page>
-      <ButtonWrap>
-        <Login onClick={toLogin}>Log in</Login>
-        <Register onClick={toRegister}>Register</Register>
-      </ButtonWrap>
+    <ButtonWrap>
+    <Login onClick={toLogin}>Log in</Login>
+    <Register onClick={toRegister}>Register</Register>
+  </ButtonWrap>
+    <Particles 
+    params={ particleOpt } style={{
+      "z-index": "-1"
+    }}
+    />
     </Page>
   );
 };
